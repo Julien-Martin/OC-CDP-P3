e527ac1970184ec8294b4c3a642262d6a54bcc9a
Nitram <contact@nitramj.fr>
2018-09-17 11:30:36 +0200

    Error handler

--- core/Router.php
+++ core/Router.php
@@ -11,18 +11,33 @@ class Router {
     {
         $homeController = new HomeController();
         $singleController = new SingleController();
-        if(isset($_GET['action'])){
-            if($_GET['action'] == 'posts'){
-                $homeController->getAll();
-            } elseif ($_GET['action'] == 'post'){
-                if(isset($_GET['id']) && $_GET['id'] > 0){
-                    $singleController->getPost();
-                } else {
-                    echo 'Erreur : aucun identifiant de billet envoyé';
+        try {
+            if(isset($_GET['action'])){
+                if($_GET['action'] == 'posts'){
+                    $homeController->getAll();
+                } elseif($_GET['action'] == 'post'){
+                    if(isset($_GET['id']) && $_GET['id'] > 0){
+                        $singleController->getPost();
+                    } else {
+                        throw new \Exception('Aucun identifiant de billet envoyé');
+                    }
+                } elseif($_GET['action'] == 'addComment'){
+                    if(isset($_GET['id']) && $_GET['id'] > 0){
+                        if(!empty($_POST['author']) && !empty($_POST['comment'])){
+                            $singleController->addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                        } else {
+                            throw new \Exception('Tous les champs ne sont pas remplis');
+                        }
+                    } else {
+                        throw new \Exception('Aucun identifiant de billet envoyé');
+                    }
                 }
+            } else {
+                $homeController->getAll();
             }
-        } else {
-            $homeController->getAll();
+        } catch (\Exception $exception){
+            $errorMessage = $exception->getMessage();
+            require 'views/front/error.php';
         }
     }
 }
--- models/Manager.php
+++ models/Manager.php
@@ -7,15 +7,11 @@ class Manager {
     private $_host = 'localhost';
     private $_dbname = 'p3';
     private $_username = 'root';
-    private $_password = 'root';
+    private $_password = '';
 
     protected function dbConnection(){
-        try {
-            $db = new \PDO('mysql:host='.$this->_host.';dbname='.$this->_dbname.';charset=utf8', $this->_username, $this->_password);
-            return $db;
-        } catch (\Exception $e){
-            echo 'Database error : '.$e->getMessage();
-        }
+        $db = new \PDO('mysql:host='.$this->_host.';dbname='.$this->_dbname.';charset=utf8', $this->_username, $this->_password);
+        return $db;
     }
 
 }
